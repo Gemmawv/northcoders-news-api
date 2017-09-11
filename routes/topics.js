2373bf594637e496e1cd4601cbc19b864ae5a435
Gemma <gemmawvoakes@gmail.com>
2017-09-11 15:25:57 +0100

    [topics] route set up.

--- routes/topics.js
+++ routes/topics.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
+const models = require('../models/models');
+
+router.get('/', function (req, res, next) {
+    models.Topics.find()
+        .then((topics) => {
+            return res.status(200).json({ topics });
+        })
+        .catch(next);
+});
 
 module.exports = router;
