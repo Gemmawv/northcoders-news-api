449069f603d4d7c044721eab1d61de3de2d50ba5
Gemma <gemmawvoakes@gmail.com>
2017-09-11 16:33:37 +0100

    GET [topics/:topic_id/articles] route tested. Returns all articles that match the given topic. Returns 404 if the topic id does not exist.

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -9,37 +9,61 @@ mongoose.Promise = global.Promise;
 const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 
 describe('API', function () {
-    beforeEach((done) => {
-        mongoose.connection.dropDatabase()
-            .then(() => saveTestData(db, function (err) {
-                if (err) throw err;
-                done();
-            }));
-    });
-    describe('GET /', function () {
-        it('responds with status code 200', function (done) {
-            request(server)
-                .get('/')
-                .end((err, res) => {
-                    if (err) done(err);
-                    else {
-                        expect(res.status).to.equal(200);
-                        done();
-                    }
-                });
-        });
-    });
-    describe('GET /topics', function () {
-        it('responds with all topics', function (done) {
-            request(server)
-                .get('/api/topics')
-                .end((err, res) => {
-                    if (err) return console.log(err);
-                    expect(res.status).to.equal(200);
-                    expect(res.body.topics.length).to.equal(3);
-                    done();
+  beforeEach((done) => {
+    mongoose.connection.dropDatabase()
+      .then(() => saveTestData(db, function (err) {
+        if (err) throw err;
+        done();
+      }));
+  });
 
-                });
+  describe('GET /', function () {
+    it('responds with status code 200', function (done) {
+      request(server)
+        .get('/')
+        .end((err, res) => {
+          if (err) done(err);
+          else {
+            expect(res.status).to.equal(200);
+            done();
+          }
         });
     });
+  });
+
+  describe('GET /topics', function () {
+    it('responds with all topics', function (done) {
+      request(server)
+        .get('/api/topics')
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.topics.length).to.equal(3);
+          done();
+        });
+    });
+  });
+  
+  describe('GET /api/topics/:topic_id/articles', function () {
+    it('should return all of the articles that match the requested topic', function (done) {
+      request(server)
+        .get('/api/topics/football/articles')
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.articlesByTopic.length).to.equal(1);
+          done();
+        });
+    });
+    it('should return an error if the topic does not exist', function (done) {
+      request(server)
+        .get('/api/topics/coconuts/articles')
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(404);
+          expect(res.body.message).to.equal('Topic not found');
+          done();
+        });
+    });
+  });
 });
