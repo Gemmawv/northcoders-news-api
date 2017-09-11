5a52be7a05830ef3b51f1f9aeb1d3e66215ca4b2
Gemma <gemmawvoakes@gmail.com>
2017-09-11 15:34:34 +0100

    GET [topics] route tested.

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -29,4 +29,17 @@ describe('API', function () {
                 });
         });
     });
+    describe('GET /topics', function () {
+        it('responds with all topics', function (done) {
+            request(server)
+                .get('/api/topics')
+                .end((err, res) => {
+                    if (err) return console.log(err);
+                    expect(res.status).to.equal(200);
+                    expect(res.body.topics.length).to.equal(3);
+                    done();
+
+                });
+        });
+    });
 });
