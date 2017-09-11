75e3affe45291d6514fc50ad8ec0afa4a83bb9cd
Gemma <gemmawvoakes@gmail.com>
2017-09-11 16:44:17 +0100

    GET[/api/articles] route set up to return all articles.

--- routes/articles.js
+++ routes/articles.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
+const models = require('../models/models');
+
+router.get('/', function (req, res, next) {
+    models.Articles.find()
+        .then((articles) => {
+            return res.status(200).json({ articles });
+        })
+        .catch(next);
+});
 
 module.exports = router;
