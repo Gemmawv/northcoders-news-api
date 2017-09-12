b90d2c8e4f30d448e933138dfbae720d4ed1cdb1
Gemma <gemmawvoakes@gmail.com>
2017-09-12 13:05:48 +0100

    POST[/api/articles/:article_id/comments] route added. Adds a new comment to an article.

--- routes/articles.js
+++ routes/articles.js
@@ -21,4 +21,24 @@ router.get('/:article_id/comments', function (req, res, next) {
         .catch(next);
 });
 
+router.post('/:article_id/comments', function (req, res, next) {
+    models.Articles.findById(req.params.article_id, (err, article) => {
+        if (err) return next(err);
+        if (article === null) {
+            const error = new Error(`Cannot find article ${req.params.article_id}`);
+            error.status = 404;
+            return next(error);
+        }
+        let newComment = new models.Comments({
+            body: req.body.body,
+            belongs_to: req.params.article_id
+        });
+        newComment.save()
+            .then((newComment) => {
+                res.status(201).json({ comment: newComment });
+            })
+            .catch(next);
+    });
+});
+
 module.exports = router;
