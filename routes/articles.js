34e2ac617f38c511102481c84b6ff86195f232b7
Gemma <gemmawvoakes@gmail.com>
2017-09-21 17:17:52 +0100

    Tidied up code in articles and comments routes.

--- routes/articles.js
+++ routes/articles.js
@@ -22,36 +22,34 @@ router.get('/:article_id/comments', function (req, res, next) {
 });
 
 router.post('/:article_id/comments', function (req, res, next) {
-    models.Articles.findById(req.params.article_id, (err, article) => {
-        if (err) return next(err);
-        if (article === null) {
-            const error = new Error(`Cannot find article ${req.params.article_id}`);
-            error.status = 404;
-            return next(error);
-        }
-        let newComment = new models.Comments({
-            body: req.body.body,
-            belongs_to: req.params.article_id
-        });
-        newComment.save()
-            .then((newComment) => {
-                res.status(201).json({ comment: newComment });
-            })
-            .catch(next);
-    });
+    const _id = req.params.article_id;
+    models.Articles.findById(_id)
+        .then((article) => {
+            if (article === null) {
+                const error = new Error(`Cannot find article ${_id}`);
+                error.status = 404;
+                throw error;
+            }
+            const newComment = new models.Comments({
+                body: req.body.body,
+                belongs_to: _id
+            });
+            newComment.save();
+            res.status(201).json({ comment: newComment });
+        })
+        .catch(next);
+
 });
 
 router.put('/:article_id', function (req, res, next) {
     const _id = req.params.article_id;
-    models.Articles.findOne({ _id }, (err, article) => {
-        if (err) return next(err);
-        if (article === null) {
-            const error = new Error(`Cannot find article ${req.params.article_id}`);
-            error.status = 404;
-            return next(error);
-        }
-    })
+    models.Articles.findOne({ _id })
         .then((article) => {
+            if (article === null) {
+                const error = new Error(`Cannot find article ${_id}`);
+                error.status = 404;
+                throw error;
+            }
             if (req.query.vote === 'up') article.votes += 1;
             if (req.query.vote === 'down') article.votes -= 1;
             return article.save();
--- routes/comments.js
+++ routes/comments.js
@@ -3,15 +3,13 @@ const models = require('../models/models');
 
 router.put('/:comment_id', function (req, res, next) {
     const _id = req.params.comment_id;
-    models.Comments.findOne({ _id }, (err, comment) => {
-        if (err) return next(err);
-        if (comment === null) {
-            const error = new Error(`Cannot find comment ${req.params.comment_id}`);
-            error.status = 404;
-            return next(error);
-        }
-    })
+    models.Comments.findOne({ _id })
         .then((comment) => {
+            if (comment === null) {
+                const error = new Error(`Cannot find comment ${_id}`);
+                error.status = 404;
+                throw error;
+            }
             if (req.query.vote === 'up') comment.votes += 1;
             if (req.query.vote === 'down') comment.votes -= 1;
             return comment.save();
@@ -24,15 +22,13 @@ router.put('/:comment_id', function (req, res, next) {
 
 router.delete('/:comment_id', function (req, res, next) {
     const id = req.params.comment_id;
-    models.Comments.findOneAndRemove({ belongs_to: id }, (err, comment) => {
-        if (err) return next(err);
-        if (comment === null) {
-            const error = new Error(`Cannot find comment ${id}`);
-            error.status = 404;
-            return next(error);
-        }
-    })
-        .then(() => {
+    models.Comments.findOneAndRemove({ _id: id })
+        .then((comment) => {
+            if (comment === null) {
+                const error = new Error(`Cannot find comment ${id}`);
+                error.status = 404;
+                throw error;
+            }
             res.status(200).json({ message: 'Comment has been deleted.' });
         })
         .catch(next);
