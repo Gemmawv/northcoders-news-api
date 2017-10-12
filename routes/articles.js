3db9414292722163baeb2e4630e8db930adaa4f7
Gemma <gemmawvoakes@gmail.com>
2017-10-12 15:35:22 +0100

    [/api/topics/:topic_id/articles] now has access to avatar urls.

--- routes/articles.js
+++ routes/articles.js
@@ -2,116 +2,116 @@ const router = require('express').Router();
 const models = require('../models/models');
 
 router.get('/', function (req, res, next) {
-    models.Articles.find()
-        .then((articles) => {
-            return Promise.all([articles, ...articles.map((article) => {
-                return models.Users.findOne({ username: article.created_by });
-            })]);
-        })
-        .then(([articles, ...users]) => {
-            return articles.map((article, i) => {
-                return Object.assign({}, article.toObject(), {
-                    avatar_url: users[i].avatar_url
-                });
-            });
-        })
-        .then((articles) => {
-            return res.status(200).json({ articles });
-        })
-        .catch(next);
+  models.Articles.find()
+    .then((articles) => {
+      return Promise.all([articles, ...articles.map((article) => {
+        return models.Users.findOne({ username: article.created_by });
+      })]);
+    })
+    .then(([articles, ...users]) => {
+      return articles.map((article, i) => {
+        return Object.assign({}, article.toObject(), {
+          avatar_url: users[i].avatar_url
+        });
+      });
+    })
+    .then((articles) => {
+      return res.status(200).json({ articles });
+    })
+    .catch(next);
 });
 
 router.get('/:article_id', function (req, res, next) {
-    const articleId = req.params.article_id;
-    let article;
-    models.Articles.findById(articleId)
-        .then((articleDoc) => {
-            if (articleDoc === null) {
-                return next({ status: 404, message: 'Article not found' });
-            }
-            article = articleDoc.toObject();
-            return models.Users.findOne({ username: article.created_by });
-        })
-        .then((user) => {
-            return Object.assign({}, article, {
-                avatar_url: user.avatar_url
-            });
-        })
-        .then((article) => {
-            res.status(200).json({ article });
-        })
-        .catch(next);
+  const articleId = req.params.article_id;
+  let article;
+  models.Articles.findById(articleId)
+    .then((articleDoc) => {
+      if (articleDoc === null) {
+        return next({ status: 404, message: 'Article not found' });
+      }
+      article = articleDoc.toObject();
+      return models.Users.findOne({ username: article.created_by });
+    })
+    .then((user) => {
+      return Object.assign({}, article, {
+        avatar_url: user.avatar_url
+      });
+    })
+    .then((article) => {
+      res.status(200).json({ article });
+    })
+    .catch(next);
 });
 
 router.get('/:article_id/comments', function (req, res, next) {
-    const articleId = req.params.article_id;
-    models.Comments.find({ belongs_to: articleId })
-        .then((commentsForArticles) => {
-            if (commentsForArticles.length < 1) {
-                return next({ status: 404, message: 'Article not found' });
-            }
-            return Promise.all([commentsForArticles, ...commentsForArticles.map((comment) => {
-                return models.Users.findOne({ username: comment.created_by });
-            })]);
-        })
-        .then(([commentsForArticles, ...users]) => {
-            return commentsForArticles.map((comment, i) => {
-                return Object.assign({}, comment.toObject(), {
-                    avatar_url: users[i].avatar_url
-                });
-            });
-        })
-        .then((commentsForArticles) => {
-            res.status(200).json({ commentsForArticles });
-        })
-        .catch(next);
+  const articleId = req.params.article_id;
+  models.Comments.find({ belongs_to: articleId })
+    .then((commentsForArticles) => {
+      if (commentsForArticles.length < 1) {
+        return next({ status: 404, message: 'Article not found' });
+      }
+      return Promise.all([commentsForArticles, ...commentsForArticles.map((comment) => {
+        return models.Users.findOne({ username: comment.created_by });
+      })]);
+    })
+    .then(([commentsForArticles, ...users]) => {
+      return commentsForArticles.map((comment, i) => {
+        return Object.assign({}, comment.toObject(), {
+          avatar_url: users[i].avatar_url
+        });
+      });
+    })
+    .then((commentsForArticles) => {
+      res.status(200).json({ commentsForArticles });
+    })
+    .catch(next);
 });
 
 router.post('/:article_id/comments', function (req, res, next) {
-    const _id = req.params.article_id;
-    models.Articles.findById(_id)
-        .then((article) => {
-            if (article === null) {
-                const error = new Error(`Cannot find article ${_id}`);
-                error.status = 404;
-                throw error;
-            }
-            let newComment = new models.Comments({
-                body: req.body.body,
-                belongs_to: _id
-            });
-            newComment.save()
-                .then(() => {
-                    return models.Users.findOne({ username: newComment.created_by });
-                })
-                .then((user) => {
-                    newComment = Object.assign({}, newComment.toObject(), {
-                        avatar_url: user.avatar_url
-                    });
-                    res.status(201).json({ comment: newComment });
-                });
+  const _id = req.params.article_id;
+  models.Articles.findById(_id)
+    .then((article) => {
+      if (article === null) {
+        const error = new Error(`Cannot find article ${_id}`);
+        error.status = 404;
+        throw error;
+      }
+      let newComment = new models.Comments({
+        body: req.body.body,
+        belongs_to: _id
+      });
+      newComment.save()
+        .then(() => {
+          return models.Users.findOne({ username: newComment.created_by });
         })
-        .catch(next);
+        .then((user) => {
+          newComment = Object.assign({}, newComment.toObject(), {
+            avatar_url: user.avatar_url
+          });
+          res.status(201).json({ comment: newComment });
+        });
+    })
+    .catch(next);
 
 });
 
 router.put('/:article_id', function (req, res, next) {
-    const _id = req.params.article_id;
-    models.Articles.findOne({ _id })
-        .then((article) => {
-            if (article === null) {
-                const error = new Error(`Cannot find article ${_id}`);
-                error.status = 404;
-                throw error;
-            }
-            if (req.query.vote === 'up') article.votes += 1;
-            if (req.query.vote === 'down') article.votes -= 1;
-            return article.save();
-        })
-        .then((article) => {
-            res.status(201).json({ article });
-        })
-        .catch(next);
+  const _id = req.params.article_id;
+  models.Articles.findOne({ _id })
+    .then((article) => {
+      if (article === null) {
+        const error = new Error(`Cannot find article ${_id}`);
+        error.status = 404;
+        throw error;
+      }
+      if (req.query.vote === 'up') article.votes += 1;
+      if (req.query.vote === 'down') article.votes -= 1;
+      return article.save();
+    })
+    .then((article) => {
+      res.status(201).json({ article });
+    })
+    .catch(next);
 });
 
 module.exports = router;
--- routes/topics.js
+++ routes/topics.js
@@ -2,23 +2,35 @@ const router = require('express').Router();
 const models = require('../models/models');
 
 router.get('/', function (req, res, next) {
-    models.Topics.find()
-        .then((topics) => {
-            return res.status(200).json({ topics });
-        })
-        .catch(next);
+  models.Topics.find()
+    .then((topics) => {
+      return res.status(200).json({ topics });
+    })
+    .catch(next);
 });
 
 router.get('/:topic_id/articles', function (req, res, next) {
-    const slug = req.params.topic_id;
-    models.Articles.find({ belongs_to: slug })
-        .then((articlesByTopic) => {
-            if (articlesByTopic.length < 1) {
-                return next({ status: 404, message: 'Topic not found' });
-            }
-            res.status(200).json({ articlesByTopic });
-        })
-        .catch(next);
+  const topicId = req.params.topic_id;
+  models.Articles.find({ belongs_to: topicId })
+    .then((articlesByTopic) => {
+      if (articlesByTopic.length < 1) {
+        return next({ status: 404, message: 'Topic not found' });
+      }
+      return Promise.all([articlesByTopic, ...articlesByTopic.map((article) => {
+        return models.Users.findOne({ username: article.created_by });
+      })]);
+    })
+    .then(([articlesByTopic, ...users]) => {
+      return articlesByTopic.map((article, i) => {
+        return Object.assign({}, article.toObject(), {
+          avatar_url: users[i].avatar_url
+        });
+      });
+    })
+    .then((articlesByTopic) => {
+      res.status(200).json({ articlesByTopic });
+    })
+    .catch(next);
 });
 
 module.exports = router;
